744cb468d8a85636854e8f6b666a9cee8fc25457
ivarkristian <ivar.waarum@gmail.com>
2025-05-21 10:48:56 +0200

    Set debug=False

--- rl_conv_main.py
+++ rl_conv_main.py
@@ -44,7 +44,7 @@ if not os.path.exists(models_dir):
 if not os.path.exists(logdir):
 	os.makedirs(logdir)
 
-env = rl_gas_survey_env.GasSurveyEnv(bank, gp_pred_resolution=[100, 100], r_weights=[10**-5, 0.0], timer=False, debug=True)
+env = rl_gas_survey_env.GasSurveyEnv(bank, gp_pred_resolution=[100, 100], r_weights=[10**-5, 0.0], timer=False, debug=False)
 
 # %%
 #agent = PPO('MlpPolicy', env, verbose=1, n_steps=4, batch_size=2, n_epochs=2)
