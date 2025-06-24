0cd797c40e04384f2623472e38979783fe20a433
ivarkristian <ivar.waarum@gmail.com>
2025-06-24 15:34:02 +0200

    Readying train_freq=4

--- rl_conv_disc_main.py
+++ rl_conv_disc_main.py
@@ -51,7 +51,7 @@ if device is None:
 
 #action_mode = {'absolute', 250, 250}
 action_mode = ['relative', 20, 20]
-channels = np.array([1, 1, 0, 1, 1])
+channels = np.array([0, 1, 0, 0, 0])
 env = rl_gas_survey_discrete_env.GasSurveyDiscEnv(bank, gp_pred_resolution=[100, 100], r_weights=[2.0, 3.0], channels=channels, action_mode=action_mode, timer=False, debug=False, device=device)
 
 buffer_size = 800_000                      # how many transitions
@@ -117,7 +117,7 @@ else:
         learning_rate=3e-4,
         learning_starts=256,
         tau=0.005,
-        train_freq=1,
+        train_freq=4,
         gradient_steps=1,
         policy_kwargs=policy_kwargs,
         verbose=1,
