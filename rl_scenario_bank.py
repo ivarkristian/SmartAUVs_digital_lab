2a7ec69b563d4089efa20ff56001606761d9d906
ivarkristian <ivar.waarum@gmail.com>
2025-12-15 14:47:48 +0100

    Added changes from remote branch...

--- rl_scenario_bank.py
+++ rl_scenario_bank.py
@@ -76,7 +76,6 @@ class ScenarioBank:
         cur_str = np.sqrt(u.mean()**2 + v.mean()**2)
 
         metadata = {'parameter': parameter, 'depth': depth, 'time': str(self.dataset['time'].values[0]).split('T')[1].split('.')[0], 'cur_dir': cur_dir, 'cur_str': cur_str, 'data_file': self.data_file}
-
         return env_xy, torch.tensor(values), metadata
     
     def add_env(self, parameter='pH', depth=67, time=1):
